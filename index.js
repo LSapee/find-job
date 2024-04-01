3ca11c1a8b4cfe3d1b7ce7a60a11de52b4a07540
LSapee <dlworhks94@gmail.com>
2024-04-01 18:19:21 +0900

    ec2 테스트용으로 업데이트

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ const hasNextPage = async (driver,targetSite)=>{
 app.get("/jobk",async (req,res)=>{
     // 브라우저 크롬 세팅
     let driver = await new Builder().forBrowser("chrome").setChromeOptions(chromeOptions).build();
-    // let driver = await new Builder().forBrowser("chrome").build();
+    // let driver = await new Builder().forBrowser("chrome").build();z
     // return 해줄 배열
     let myList =[];
     // 구분자로 쓸 예정
