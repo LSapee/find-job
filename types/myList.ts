d86df11070607ec0cb80b8021f92e5a145677666
LSapee <dlworhks94@gmail.com>
2024-04-12 02:02:40 +0900

    type주석 추가

--- types/myList.ts
+++ types/myList.ts
@@ -8,3 +8,13 @@ export type MyList = {
     endDate : string
     postURL :string|boolean
 }
+/*
+* company = 회사명
+* postTitle = 공고명
+* exp = 경력 n~m년 배열
+* edu = 학력
+* loc = 지역
+* skillStacks = 기술스택
+* endDate = 채용 마감일
+* postURL = URL
+* */
