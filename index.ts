8efcdafa0365abcb0c46db12c68d24564b1d8a78
LSapee <dlworhks94@gmail.com>
2024-05-05 17:56:53 +0900

    idToken 확인용 콘솔 출력

--- index.ts
+++ index.ts
@@ -25,6 +25,7 @@ crawlingScheduler();
 app.get("/api/auth",async (req:Request,res:Response)=>{
     const {code:code}= req.query;
     const tokens  = await getToken(code);
+    console.log("idToken",tokens.id_token);
     const loginTF  = await loginUser(tokens.id_token);
     if(loginTF) {
         if(tokens!==null ||tokens!==undefined){
@@ -43,23 +44,16 @@ app.get("/api/auth",async (req:Request,res:Response)=>{
     }
     res.redirect("https://findjob.lsapee.com");
 });
-//로그아웃시
-app.get("/api/logout",async (req:Request,res:Response)=>{
-    res.clearCookie("access_token",{domain: '.lsapee.com'});
-    res.clearCookie("access",{domain: '.lsapee.com'});
-   res.redirect("https://findjob.lsapee.com");
-});
+
 // DB 조회
 app.get("/api/getjob",cookieParser(), async (req:Request,res:Response)=>{
+    // access_token 가져오기
     const access_Token = req.cookies["access_token"];
-    const userEmail = await getEmail(access_Token)
-    console.log("userEmail",userEmail)
     const {search:keyword,expAll:expAll,exp:myExp,startNum:startNum} = req.query;
     // 추가 조회할 정보 데이터 시작번호
     let stnum:number =0;
     if(typeof(startNum)==="string")  stnum = parseInt(startNum);
     const myList:MyList|boolean = await findAlljob(keyword,expAll,myExp,stnum);
-    const accessToken =req.cookies["access_token"];
     res.send(myList);
 })
 //등록된 키워드 데이터 가져오기
@@ -67,6 +61,12 @@ app.get("/api/getKeywords",async (req:Request,res:Response)=>{
     const keywords = await findAllkeyWords();
     res.send(keywords);
 })
+//로그아웃시
+app.get("/api/logout",async (req:Request,res:Response)=>{
+    res.clearCookie("access_token",{domain: '.lsapee.com'});
+    res.clearCookie("access",{domain: '.lsapee.com'});
+    res.redirect("https://findjob.lsapee.com");
+});
 
 //메인 페이지
 app.get("/",(req:Request,res:Response)=>{
