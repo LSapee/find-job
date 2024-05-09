b6aa4101c655200f500c7bc3014d52497e2e8d71
LSapee <dlworhks94@gmail.com>
2024-05-09 22:50:43 +0900

    EC2 실행 및 중지 스케쥴러 추가

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import {requireLogin} from "./middleware/requireLogin";
 const app = express();
 const {findAlljob,findAllkeyWords} = require("./Repository/findjob.Repository")
 const {loginUser,saveTokens,deleteAccessToken} = require("./Repository/user.Repository");
-const {crawlingScheduler} = require("./utils/scheduler")
+const {ec2StopTimet,ec2StartTimet} = require("./utils/scheduler")
 const {neverSee,neverSeeCompanys,delneverSeeCompany} = require("./Repository/compony.Repository")
 const {application_completed,application_completed_companyList,application_completed_company_cen,application_completed_company_write} = require("./Repository/application_completed_company.Repository")
 const {getToken} = require("./auth/auth");
@@ -29,8 +29,8 @@ app.use(express.json());
 app.use(cookieParser());
 // urlparam 파싱
 app.use(express.urlencoded({ extended: true }));
-// 자동 크롤링
-crawlingScheduler();
+ec2StartTimet();
+ec2StopTimet();
 //로그인시
 app.get("/api/auth",async (req:Request,res:Response)=>{
     const {code:code}= req.query;
@@ -127,6 +127,8 @@ app.post("/api/appCom",requireLogin,async (req:Request,res:Response)=>{
     if(success===false)res.send({success:"추가 실패"})
     else res.send(success)
 })
+
+
 
 //임시페이지
 app.get("/",(req:Request,res:Response)=>{
--- utils/scheduler.ts
+++ utils/scheduler.ts
@@ -1,17 +1,70 @@
 const cron = require("node-cron");
 const {findKeywords,postDel} = require("../Repository/crawler.Repository")
 const {jobKCrawler,saramInCrawler} =  require("../crawler/crawler")
+import { EC2Client, StartInstancesCommand ,StopInstancesCommand} from "@aws-sdk/client-ec2";
 
-// 분 시 일 월 요일
-export const crawlingScheduler = async ()=>{
-    cron.schedule(("0 0 * * *"), async () =>{
-        await postDel();
-        const keywords : string[] = await findKeywords();
-        for(const item of keywords){
-            const resultJok = await jobKCrawler(item);
-            const resultSaramIn = await saramInCrawler(item);
-            console.log(`크롤링 resultJob ${item}:`, resultJok );
-            console.log(`크롤링 resultSaramIn ${item}:`, resultSaramIn );
-        }
+const awsRegion:string = process.env.AWS_REGION as string;
+const awsInstanceId:string = process.env.AWS_EC2_INSTENT_ID as string;
+console.log(awsInstanceId)
+// EC2Client 객체 생성
+const ec2Client = new EC2Client({ region: awsRegion });
+
+// EC2 인스턴스 시작 요청
+const params = {
+    InstanceIds: [awsInstanceId], // 시작할 인스턴스의 ID
+};
+
+const startCommand = new StartInstancesCommand(params);
+const stopCommand = new StopInstancesCommand(params);
+
+export const ec2Start = async  () =>{
+    try{
+        ec2Client.send(startCommand)
+            .then((data) => {
+                console.log("인스턴스 시작 요청 : ", data);
+            })
+            .catch(err => {
+                console.error("인스턴스 시작 중 오류 발생:", err);
+            });
+    }catch (e){
+        console.log("e",e);
+    }
+}
+export const ec2Stop = async  () =>{
+    try {
+        ec2Client.send(stopCommand)
+            .then((data) => {
+                console.log("인스턴스 중지 요청 : ", data);
+            })
+            .catch(err => {
+                console.error("인스턴스 중지 중 오류 발생:", err);
+            });
+    }catch (e){
+        console.log("e",e);
+    }
+}
+
+export const ec2StartTimet = async ()=>{
+    cron.schedule(("53 22 * * *"), async () =>{
+        ec2Start();
     })
 }
+export const ec2StopTimet = async ()=>{
+    cron.schedule(("0 23 * * *"), async () =>{
+        ec2Stop();
+    })
+}
+
+// 분 시 일 월 요일
+// export const crawlingScheduler = async ()=>{
+//     cron.schedule(("0 0 * * *"), async () =>{
+//         await postDel();
+//         const keywords : string[] = await findKeywords();
+//         for(const item of keywords){
+//             const resultJok = await jobKCrawler(item);
+//             const resultSaramIn = await saramInCrawler(item);
+//             console.log(`크롤링 resultJob ${item}:`, resultJok );
+//             console.log(`크롤링 resultSaramIn ${item}:`, resultSaramIn );
+//         }
+//     })
+// }
