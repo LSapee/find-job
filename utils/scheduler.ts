5e27207817306b7ee8ed1170a20b07b52ba6adb0
LSapee <dlworhks94@gmail.com>
2024-05-10 18:21:47 +0900

    EC2 연결 확인 테스트

--- utils/scheduler.ts
+++ utils/scheduler.ts
@@ -87,7 +87,7 @@ const startPm2OnEC2Instance = async ()=> {
 }
 // EC2가 켜질 타이밍
 export const ec2StartTimet = async ()=>{
-    cron.schedule(("06 22 * * *"), async () =>{
+    cron.schedule(("25 18 * * *"), async () =>{
         // await ec2Start();
         // setTimeout(async () => {
             await startPm2OnEC2Instance();
