5dd09b1bde8804e6af6b8cd2874c947cc12a6552
LSapee <dlworhks94@gmail.com>
2024-05-10 18:02:07 +0900

    EC2 연결 확인 테스트

--- utils/scheduler.ts
+++ utils/scheduler.ts
@@ -1,7 +1,5 @@
 const cron = require("node-cron");
-const {findKeywords,postDel} = require("../Repository/crawler.Repository")
-const {jobKCrawler,saramInCrawler} =  require("../crawler/crawler")
-import {DescribeInstancesCommand, EC2Client, StartInstancesCommand, StopInstancesCommand} from "@aws-sdk/client-ec2";
+import {DescribeInstancesCommand, EC2Client, StartInstancesCommand} from "@aws-sdk/client-ec2";
 const { exec } = require("child_process");
 
 const awsRegion:string = process.env.AWS_REGION as string;
@@ -74,7 +72,7 @@ const ec2Start = async  () =>{
 // EC2에서 pm2 실행
 const startPm2OnEC2Instance = async ()=> {
     // SSH로 EC2 인스턴스에 연결하여 pm2를 실행
-    const sshCommand = `ssh -i ${myPemKey} ubuntu@${process.env.MY_INSTANCE_IP} "pm2 start ./find-job-craling-only/node_modules/.bin/ts-node --name "find-job" -- index.ts"`;
+    const sshCommand = `ssh -i "${myPemKey}" ubuntu@${process.env.MY_INSTANCE_IP} "pm2 start ./find-job-craling-only/node_modules/.bin/ts-node --name "find-job" -- index.ts"`;
     exec(sshCommand, (error:any, stdout:any, stderr:any) => {
         if (error) {
             console.error(`pm2 실행 중 오류 발생: ${error.message}`);
@@ -89,10 +87,10 @@ const startPm2OnEC2Instance = async ()=> {
 }
 // EC2가 켜질 타이밍
 export const ec2StartTimet = async ()=>{
-    cron.schedule(("58 17 * * *"), async () =>{
-        await ec2Start();
-        setTimeout(async () => {
+    cron.schedule(("03 18 * * *"), async () =>{
+        // await ec2Start();
+        // setTimeout(async () => {
             await startPm2OnEC2Instance();
-        }, 2 * 60 * 1000);
+        // }, 2 * 60 * 1000);
     })
 }
