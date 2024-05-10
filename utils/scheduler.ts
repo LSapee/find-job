0a6702271f33a1fdcadb0df8f1cc014fcfd071f9
LSapee <dlworhks94@gmail.com>
2024-05-10 17:54:49 +0900

    EC2 실행 테스트

--- utils/scheduler.ts
+++ utils/scheduler.ts
@@ -74,7 +74,7 @@ const ec2Start = async  () =>{
 // EC2에서 pm2 실행
 const startPm2OnEC2Instance = async ()=> {
     // SSH로 EC2 인스턴스에 연결하여 pm2를 실행
-    const sshCommand = `ssh -i "${myPemKey}" ubuntu@${process.env.MY_INSTANCE_IP} "pm2 start ./find-job-craling-only/node_modules/.bin/ts-node --name "find-job" -- index.ts"`;
+    const sshCommand = `ssh -i ${myPemKey} ubuntu@${process.env.MY_INSTANCE_IP} "pm2 start ./find-job-craling-only/node_modules/.bin/ts-node --name "find-job" -- index.ts"`;
     exec(sshCommand, (error:any, stdout:any, stderr:any) => {
         if (error) {
             console.error(`pm2 실행 중 오류 발생: ${error.message}`);
@@ -89,7 +89,7 @@ const startPm2OnEC2Instance = async ()=> {
 }
 // EC2가 켜질 타이밍
 export const ec2StartTimet = async ()=>{
-    cron.schedule(("40 17 * * *"), async () =>{
+    cron.schedule(("58 17 * * *"), async () =>{
         await ec2Start();
         setTimeout(async () => {
             await startPm2OnEC2Instance();
